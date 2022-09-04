d22f7dacc082eca0bdbeac065b6c00088ae4853d
Richard PAYET <rick.info.rsoc@gmail.com>
2022-09-04 16:32:15 +0200

    Isolation des fichiers de config. de WebPack, et séparation en config. prod et config. dev. + qq petites modifs de commentaires.

---
+++ zMyWebPackConfigs/webpack.config.prod.js
@@ -0,0 +1,26 @@
+const MyCommonConfigurationClass = require("./webpack.config.common.js");
+
+
+class MyProdConfigurationClass extends MyCommonConfigurationClass {
+    getSpecificConfig() { //Pattern Template Method
+        const oProdConfig = {
+            mode: "production"
+        };
+        return oProdConfig;
+    }
+}
+
+
+module.exports = (webPackEnvironmentVars) => { //Pour les WebPack config., lorsque module.exports 
+                                               // renvoie une lambda, celle-ci peut recevoir en param. 
+                                               // les vars d'env. WebPack.
+                                               //(vars qui comprennent également celles perso. settées
+                                               // sur la ligne de commande WebPack via param. --env MaVar=toto)
+
+    const oConfig = MyCommonConfigurationClass.getCompleteConfig( //Méthode static
+        MyProdConfigurationClass, //<<<Ma Classe enfant spécifique pour Prod.
+        webPackEnvironmentVars
+    );
+
+    return oConfig; //Et bien sûr il nous faut retourner la config. !
+};
