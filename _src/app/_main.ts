be3f6424eee2b6e7f22d78dca783b824ec431516
Richard PAYET <rick.info.rsoc@gmail.com>
2024-09-18 11:34:12 +0200

    Typage et modif. commentaire

--- _src/app/_main.ts
+++ _src/app/_main.ts
@@ -11,14 +11,14 @@ export class Main {
         const n : number = 5;
         const coeff : number = 10;
 
-        const oCalculator: ICalculator = CalculatorFactory.getCalculator(coeff);
-        const result = oCalculator.getResult(5);
+        const oCalculator: ICalculator = CalculatorFactory.getCalculator(coeff); //Choix du ICalculator concret : TS ou ES6 ou ES5.
+        const result : number = oCalculator.getResult(5);
         console.log(`result=${result}`);
 
         // console.log( result === coeff*n ); //true si MyCalculator_TS
         // console.log( result === coeff*n*2 ); //true si MyCalculator_ES6
         // console.log( result === coeff*n*3 ); //true si MyCalculator_ES5
-        // oCalculator.getAutreX(); //INTERDIT: évidemment car cette méthode n'appartient pas à l'interface ICalculator ! (elle n'appartient qu'au type : MyCalculator_TS).
+        // oCalculator.getAutreX(); //INTERDIT: évidemment car cette méthode n'appartient pas à l'interface ICalculator ! (elle n'appartient qu'au type concret : MyCalculator_xxx).
 
     }
     
