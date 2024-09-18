fa4cc4cbe864372ab4a0890caed1b14a001e5940
Richard PAYET <rick.info.rsoc@gmail.com>
2024-09-18 11:39:43 +0200

    même pas besoin de la syntaxe window.MyCalculator_ES5 = ...., car MyCalculator_ES5 = ...   suffit !

--- _src/lib/Calculators/implems/JS/ES5/MyCalculator_ES5.js
+++ _src/lib/Calculators/implems/JS/ES5/MyCalculator_ES5.js
@@ -5,9 +5,9 @@
 //  ICalculator et ICalculatorConstructor. (cf. .d.ts correspondants).
 
 // Plus précisément, je veux :
-//   1. que la présente classe ES5, MyCalculator_ES5, soit connue par le _main.js au moment de l'EXÉCUTION !! 
+//   1. que la présente classe ES5, MyCalculator_ES5, soit connue à l'EXÉCUTION !! 
 //      ce qui est réalisé grâce au "window.MyCalculator_ES5 =" juste ci-dessous ET au  
-//      'import "./.../MyCalculator_ES5";' fait dans le _main.ts.
+//      un 'import "./.../MyCalculator_ES5";'.
 //   1. que MyCalculator_ES5 implémente correctement ce que décrit l'interface ICalculator (TOUS typages compris),
 //      cette vérif. sera automatiquement réalisée à la compil., face et suite à l'instruction : 
 //       oMyInstance: ICalculator = new MyCalculator_ES5(...).
@@ -16,7 +16,7 @@
 //       declare let MyCalculator_ES5: ICalculatorConstructor;   dans MyCalculator_ES5_Constructor.d.ts.
 
 
-window.MyCalculator_ES5 = function(nCoeff) { //<<<<<<< syntaxe window...=.., DÉTERMINANTE pour que MyCalculator_ES5 soit connue à l'exécution.
+MyCalculator_ES5 = function(nCoeff) { //<<<<< même pas besoin de la syntaxe window.MyCalculator_ES5 = ...
     this.nCoeff = nCoeff;
 }
 
