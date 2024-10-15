ac19d8482a753b21bdb63756cf5fe9e755bed530
Richard PAYET <rick.info.rsoc@gmail.com>
2024-10-15 18:07:00 +0200

    Update packages versions (last one)

--- _installPackages.bat
+++ _installPackages.bat
@@ -6,15 +6,15 @@ REM call npm init
 REM ------- (Récup. dern. versions) ---------
 
 REM
-call npm i -D  webpack webpack-cli
+call npm i -D  webpack@latest webpack-cli@latest
 
 REM Pour la gestion des .ts par WebPack. (REM. : pas du tout besoin de faire : npm i -D typescript)
-call npm i -D  ts-loader
+call npm i -D  ts-loader@latest
 
 REM Plugin permettant que la rubrique "paths:" du tsconfig.json, soit bien prise en compte et exploitable par le code .ts.
-call npm i -D tsconfig-paths-webpack-plugin
+call npm i -D tsconfig-paths-webpack-plugin@latest
 
 
 REM Pour les styles
-call npm i -D css-loader
-call npm i -D style-loader
+call npm i -D css-loader@latest
+call npm i -D style-loader@latest
